f7ede403e1eae397566eb2304e565643027dcafb
MadWolfInTheHub <krivenko.serhii@icloud.com>
2023-06-11 15:41:04 +0800

    Filter games by genre

--- src/Components/PlatformIconList.tsx
+++ src/Components/PlatformIconList.tsx
@@ -33,7 +33,7 @@ const PlatformIconList = ({ platforms }: Props) => {
   return (
     <HStack marginY={"10px"} color="gray.500">
       {platforms.map((platform) => (
-        <Icon as={iconMap[platform.slug]}></Icon>
+        <Icon key={platform.id} as={iconMap[platform.slug]}></Icon>
       ))}
     </HStack>
   );
