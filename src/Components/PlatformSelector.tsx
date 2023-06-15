0be6b4c2aaabac6c78a87748660cc258ee771bcc
MadWolfInTheHub <krivenko.serhii@icloud.com>
2023-06-15 15:10:10 +0800

    Handle games with no image

--- src/Components/PlatformSelector.tsx
+++ src/Components/PlatformSelector.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
-  const { data, error, isLoading } = usePlatforms();
+  const { data, error } = usePlatforms();
 
   if (error) return null;
 
