feb3fba53af3a1ebe88c26081ed26312e80b6b91
MadWolfInTheHub <krivenko.serhii@icloud.com>
2023-06-23 09:41:36 +0800

    test2

--- src/Components/GameHeading.tsx
+++ src/Components/GameHeading.tsx
@@ -9,9 +9,12 @@ interface Props {
 
 const GameHeading = ({ gameQuery }: Props) => {
   const { data: genres } = useGenres();
-  const { data: platforms } = usePlatforms();
   const genre = genres?.results.find((g) => g.id === gameQuery.genreId);
-  const platform = platforms?.results.find((p) => p.id === gameQuery.genreId);
+
+  const { data: platforms } = usePlatforms();
+  const platform = platforms?.results.find(
+    (p) => p.id === gameQuery.platformId
+  );
 
   const heading = `${platform?.name || ""} ${genre?.name || ""} Games`;
 
